b66287effee247dbf778c41b18575d98d500cac9
Benetti <O013767@arg.bbva.com>
2024-12-10 17:31:42 -0300

    Reformas al home + Creacion del Carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import LoginSignUp from "./components/LoginSignUp/LoginSignUp";
 import NavBar from "./components/UI/NavBar";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home/home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/navBar" element={<NavBar/>} />
+          <Route path="/home" element={<Home/>} />
           <Route path="/login" element={<LoginSignUp isLogin={true} />} />
           <Route path="/signUp" element={<LoginSignUp isLogin={false}  />} />
         </Routes>

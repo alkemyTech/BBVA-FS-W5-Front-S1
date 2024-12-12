2bf9bff89e5c4fa140227c90661b684422ef4588
LuccaTrovato-2006 <luccasantino.trovato@bbva.com>
2024-12-12 01:13:30 -0300

    Transaccion y deposit arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginSignUp from "./components/LoginSignUp/LoginSignUp";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
+import SendMoney from "./components/SendMoney/SendMoney"
 
 
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/signUp" element={<LoginSignUp isLogin={false} />} />
           <Route path="/sendmoney" element={<SendMoney send={true}/>} />
           <Route path="/depositmoney" element={<SendMoney send={false}/>} />
+
         </Routes>
     </Page>
     </Router>
